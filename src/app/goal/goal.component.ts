538e99901c9f757a4022d3cd8a5c4038bad82695
enock <enckkipronoh@gmail.com>
2021-08-24 14:40:07 +0300

    adding the alert

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
-export class GoalComponent implements OnInit {
+export class GoalComponent implements OnInit { 
+
+  goals:Goal[];
+  alertService:AlertService;
 
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
+  addNewGoal (goal){
+  let goalLength = this.goals.length;
+  goal.id = goalLength+1;
+  goal.completeDate = new Date(goal.completeDate)
+  this.goals.push(goal);
+ }
 
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
@@ -33,17 +37,15 @@ export class GoalComponent implements OnInit {
 
       if(toDelete){
         this.goals.splice(index, 1)
+        this.alertService.alertMe("This goal has been deleted")
       }
     }
   }
 
-  addNewGoal(goal){
-    let goalLength = this.goals.length;
-    goal.id = goalLength+1;
-    goal.completeDate = new Date(goal.completeDate)
-    this.goals.push(goal)
-  }
-  constructor() { }
+  constructor(goalService:GoalService, alertService:AlertService) {
+    this.goals = goalService.getGoals()
+    this.alertService = this.alertService;
+   }
 
   ngOnInit() {
   }
